1aa1e8fdfa3011c2a70ad611563751e527f21b3f
doug24 <djpersons@gmail.com>
2020-05-03 10:53:00 -0700

    initial commit

---
+++ Sudoku/SquareViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Sudoku
+{
+    public class SquareViewModel : ViewModelBase
+    {
+        public SquareViewModel()
+        {
+            Row = 0;
+            Col = 0;
+            InitCells();
+        }
+
+        public SquareViewModel(int row, int col)
+        {
+            Row = row;
+            Col = col;
+            InitCells();
+        }
+
+        private void InitCells()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Cells.Add(new CellViewModel(row, col));
+                }
+            }
+        }
+
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        public ObservableCollection<CellViewModel> Cells { get; set; } = new ObservableCollection<CellViewModel>();
+    }
+}
---
+++ Sudoku/SudokuViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class SudokuViewModel : ViewModelBase
+    {
+        public ObservableCollection<SquareViewModel> Squares { get; set; } = new ObservableCollection<SquareViewModel>();
+
+        public SudokuViewModel()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Squares.Add(new SquareViewModel(row, col));
+                }
+            }
+        }
+    }
+}
