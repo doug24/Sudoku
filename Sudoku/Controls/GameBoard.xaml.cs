d769e9153903abc3059508eeab325288420af0e3
doug24 <djpersons@gmail.com>
2020-05-10 20:37:27 -0700

    fix(?) for cell touch

--- Sudoku/Controls/GameBoard.xaml.cs
+++ Sudoku/Controls/GameBoard.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sudoku
 {
@@ -23,6 +13,34 @@ namespace Sudoku
         public GameBoard()
         {
             InitializeComponent();
+        }
+
+        private void ListBox_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            if (e.OriginalSource is DependencyObject control)
+            {
+                var listBoxItem = FindParent<ListBoxItem>(control);
+
+                if (listBoxItem != null)
+                {
+                    gameListBox.SelectedItem = listBoxItem;
+                    e.Handled = true;
+                }
+            }
+        }
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null) return null;
+
+            //check if the parent matches the type we're looking for
+            if (parentObject is T parent)
+                return parent;
+            else
+                return FindParent<T>(parentObject);
         }
     }
 }
