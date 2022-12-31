5dbd97328792cc3b9b20119d57163b77f08f97e3
doug24 <17227248+doug24@users.noreply.github.com>
2022-12-30 16:38:23 -0800

    fix for inconsistent touch selection

--- Sudoku/Controls/GameBoard.xaml.cs
+++ Sudoku/Controls/GameBoard.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows.Controls;
 
 namespace Sudoku
 {
@@ -13,34 +10,6 @@ namespace Sudoku
         public GameBoard()
         {
             InitializeComponent();
-        }
-
-        private void ListBox_PreviewTouchDown(object sender, TouchEventArgs e)
-        {
-            if (e.OriginalSource is DependencyObject control)
-            {
-                var listBoxItem = FindParent<ListBoxItem>(control);
-
-                if (listBoxItem != null)
-                {
-                    gameListBox.SelectedItem = listBoxItem;
-                    e.Handled = true;
-                }
-            }
-        }
-        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
-        {
-            //get parent item
-            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
-
-            //we've reached the end of the tree
-            if (parentObject == null) return null;
-
-            //check if the parent matches the type we're looking for
-            if (parentObject is T parent)
-                return parent;
-            else
-                return FindParent<T>(parentObject);
         }
     }
 }
