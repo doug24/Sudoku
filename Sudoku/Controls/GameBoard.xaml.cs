a3dc57c492262ff2ddff725b79d320db8775e32d
doug24 <17227248+doug24@users.noreply.github.com>
2023-01-05 10:01:03 -0800

    finally, a fix for touch selection

--- Sudoku/Controls/GameBoard.xaml.cs
+++ Sudoku/Controls/GameBoard.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Sudoku
 {
@@ -10,6 +11,21 @@ namespace Sudoku
         public GameBoard()
         {
             InitializeComponent();
+
+            gameListBox.PreviewStylusDown += GameListBox_PreviewStylusDown;
+        }
+
+        // ListBox selection events are inconsistent from touchscreen, convert the
+        // touch events to mouse left button down events
+        // https://stackoverflow.com/questions/50488973/wpf-listviewitem-events-not-firing-properly-on-touchscreen
+        private void GameListBox_PreviewStylusDown(object sender, StylusDownEventArgs e)
+        {
+            MouseButtonEventArgs arg = new(Mouse.PrimaryDevice, 0, MouseButton.Left)
+            {
+                RoutedEvent = PreviewMouseLeftButtonDownEvent
+            };
+            gameListBox.RaiseEvent(arg);
+            e.Handled = true;
         }
     }
 }
