dd4271866d09aa4d9ba4c03ceb36f6cebbe12abe
doug24 <17227248+doug24@users.noreply.github.com>
2023-01-16 10:22:43 -0800

    rename test project

---
+++ QQWing.Tests/SectionUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using QQWingLib;
+
+namespace QQWingTest
+{
+    [TestClass]
+    public class SectionUnitTests
+    {
+        [TestMethod]
+        public void TestSectionToSectionRows()
+        {
+            ISectionLayout section = new RegularLayout();
+
+            for (int sec = 0; sec < QQWing.ROW_COL_SEC_SIZE; sec++)
+            {
+                var rows = section.SectionToSectionRows(sec).ToList();
+            }
+        }
+    }
+}
